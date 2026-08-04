3a107a18fb1f43408419ff26d2f1dfd32459ca2b
Edwin <murithiedwing@gmail.com>
2026-08-04 22:53:02 +0300

    j

---
+++ ui/src/lib/web-components/temporal-control.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { type TemporalResolution, parseDateString } from '@murithigeo/covjson-core';
+	import { type ComponentProps } from 'svelte';
+	import { Slider } from '../components/ui/slider/index.ts';
+	interface Props {
+		/**
+		 * Assumes that the values are sorted
+		 */
+		values: string[];
+		/**
+		 * The current temporal value
+		 */
+		current: string;
+		/**
+		 * How to format the current value
+		 */
+		formatter?(v: string, resolution: TemporalResolution): string;
+	}
+
+	let { values, current = $bindable(), formatter, ...props }: Props = $props();
+</script>
+
+<Slider max={values.length} step={1} />
